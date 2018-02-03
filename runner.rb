cf720720737f03e8f20ecf45a3681210901a9111
Jim Bevington <jim.bevington@hotmail.com>
2018-02-03 21:37:23 +0000

    moved clear screen inside loop to reset screen after each guess. also added display full word when game won.

--- runner.rb
+++ runner.rb
@@ -9,21 +9,23 @@ game = Game.new(player, hidden_word)
 
 game.get_hidden_word()
 
-puts %x{clear}
-
 while game.inplay do
 
+  puts %x{clear} # clear terminal screen
   # Display Info
   p "#{hidden_word.display}"
   game.display_guessed_letters()
-
+  p "You have #{player.lives} remaining guesses."
   guess = game.get_guess()
   game.parse_guess(guess)
   game.check_won_lost?
 
   if game.won
+    puts %x{clear} # clear terminal screen
+    p "#{hidden_word.display.upcase}"
     p "Congratulations! You WIN!!!"
   elsif game.lost
+    puts %x{clear} # clear terminal screen
     p "Oh dear, you LOSE!!!"
   end
 
